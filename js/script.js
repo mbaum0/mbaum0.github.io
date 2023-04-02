91bdc00b44f1b0658e2d847fb0204128b91509cf
Michael Baumgarten <mbaum0@pm.me>
2023-04-02 14:05:37 +0200

    redesign

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function drawText(i) {
-	var currentStyle = document.getElementById("hidden" + i).style.display;
-	if (currentStyle == '') {
-		document.getElementById("hidden" + i).style.display = "block";
-	} else {
-		document.getElementById("hidden" + i).style.display = "";
-	}
-}
